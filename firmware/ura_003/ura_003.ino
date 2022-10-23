ad657f97217293a452da23f2fc6d354914c9b3d5
brendacq <brenda.calado.083@ufrn.edu.br>
2022-10-23 00:48:51 -0300

    mend

--- firmware/ura_003/ura_003.ino
+++ firmware/ura_003/ura_003.ino
@@ -1,13 +1,21 @@
 #include <Ultrasonic.h>
-
+#include "MotorControl.h"
 
 byte triggerPin = 22;
 byte echoPin = 23;
 
+// MOTOR ESQUERDO
+byte motorA_1A = 27;
+byte motorA_1B = 14;
+
+//MOTOR DIREITO
+byte motorB_1A = 12;
+byte motorB_1B = 13;
+
 Ultrasonic sonar(triggerPin, echoPin);
+MotorControl andar(motorA_1A, motorA_1B, motorB_1A, motorB_1B);
 
 void setup() {
-  
 
 }
 
